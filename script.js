abbe4fffc76d08d688a4d5813823d57a1c82e5f8
Bruno Torres <br.torres.del@gmail.com>
2026-03-02 13:58:02 -0300

    Ajuste nos eventos para alterar a imagem de cada modo

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const html = document.querySelector("html");
+
 const focoBtn = document.querySelector(".app__card-button--foco");
 const curtoBtn = document.querySelector(".app__card-button--curto");
 const longoBtn = document.querySelector(".app__card-button--longo");
 
+const tempFoco = 1500, tempCurto = 300, tempLongo = 900;
+
+const timer = document.querySelector("#timer");
+const start_pause_btn = document.querySelector("#start-pause");
+
+const bannerImg = document.querySelector(".app__image");
+
+const title = document.querySelector(".app__title");
+const titleStrong = document.querySelector(".app__title-strong");
+
 focoBtn.addEventListener("click", () => {
     html.setAttribute('data-contexto', 'foco');
+    bannerImg.setAttribute("src", "./imagens/foco.png");
     focoBtn.classList.add("active");
     curtoBtn.classList.remove("active");
     longoBtn.classList.remove("active");
@@ -12,6 +24,7 @@ focoBtn.addEventListener("click", () => {
 
 curtoBtn.addEventListener("click", () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    bannerImg.setAttribute("src", "./imagens/descanso-curto.png");
     focoBtn.classList.remove("active");
     curtoBtn.classList.add("active");
     longoBtn.classList.remove("active");
@@ -19,6 +32,7 @@ curtoBtn.addEventListener("click", () => {
 
 longoBtn.addEventListener("click", () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    bannerImg.setAttribute("src", "./imagens/descanso-longo.png");
     focoBtn.classList.remove("active");
     curtoBtn.classList.remove("active");
     longoBtn.classList.add("active");

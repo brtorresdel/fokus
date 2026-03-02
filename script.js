4b8a55e58be96e851dd581f5e8fa6c9bb399f199
Bruno Torres <br.torres.del@gmail.com>
2026-03-02 15:40:38 -0300

    Criação e configurações do temporizador no JS

--- script.js
+++ script.js
@@ -6,15 +6,22 @@ const longoBtn = document.querySelector(".app__card-button--longo");
 const btnsModes = document.querySelectorAll(".app__card-button");
 
 const tempFoco = 1500, tempCurto = 300, tempLongo = 900;
+let temporizadorSeg = 5;
+let intervaloID = null;
 
 const timer = document.querySelector("#timer");
 const start_pause_btn = document.querySelector("#start-pause");
+const start_pause_btn_text = start_pause_btn.querySelector("span");
+const start_pause_btn_icon = start_pause_btn.querySelector("img");
 
 const bannerImg = document.querySelector(".app__image");
 const title = document.querySelector(".app__title");
 
 const musicaFocoInput = document.querySelector("#alternar-musica");
 const musica = new Audio("./sons/luna-rise-part-one.mp3");
+const start = new Audio("./sons/play.wav");
+const pause = new Audio("./sons/pause.mp3");
+const finalizado = new Audio("./sons/beep.mp3");
 musica.loop = true;
 
 focoBtn.addEventListener("click", () => {alterarContexto("foco")});
@@ -30,6 +37,8 @@ musicaFocoInput.addEventListener("change", () => {
 
     return musica.pause();
 });
+
+
 
 function alterarContexto (contexto) {
     html.setAttribute('data-contexto', contexto);
@@ -50,4 +59,36 @@ function alterarContexto (contexto) {
             longoBtn.classList.add("active");
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    if (temporizadorSeg <= 0){
+        // finalizado.play();
+        alert("Tempo esgotado!");
+        zerar()
+        return;
+    } 
+    temporizadorSeg--;
+    console.log(`Temporizador: ${temporizadorSeg} segundos`);
+}
+
+start_pause_btn.addEventListener("click", iniciar_pausar)
+
+function iniciar_pausar() {
+    if (intervaloID) {
+        pause.play();
+        zerar();
+        start_pause_btn_text.textContent = "Começar";
+        start_pause_btn_icon.setAttribute("src", "./imagens/play_arrow.png");
+        return;
+    }
+    start.play();
+    intervaloID = setInterval(contagemRegressiva, 1000);
+    start_pause_btn_text.textContent = "Pausar";
+    start_pause_btn_icon.setAttribute("src", "./imagens/pause.png");
+}
+
+function zerar() {
+    clearInterval(intervaloID);
+    intervaloID = null;
 }
